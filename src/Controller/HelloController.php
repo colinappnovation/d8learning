802e961da183103fbe302a5692c9c8d68a4e5b6f
Colin McClure <colin@appnovation.com>
2016-12-12 20:24:23 +0000

    Coding standards in place

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,14 +1,24 @@
 <?php
 
- namespace Drupal\hello_world\Controller;
+namespace Drupal\hello_world\Controller;
 
- use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Controller\ControllerBase;
 
- class HelloController extends ControllerBase {
-     public function content() {
-         return array(
-             '#type' => 'markup',
-             '#markup' => $this->t('Hello, World!'),
-         );
-     }
- }
+/**
+ * HelloController.
+ */
+class HelloController extends ControllerBase {
+  /**
+   * Return page content to client browser.
+   *
+   * @return array
+   *         Render array.
+   */
+  public function pageContent() {
+    return array(
+      '#type'   => 'markup',
+      '#markup' => $this->t('Hello, World!'),
+    );
+  }
+
+}
--- src/Plugin/Block/HelloBlock.php
+++ src/Plugin/Block/HelloBlock.php
@@ -7,64 +7,65 @@ use Drupal\Core\Block\BlockPluginInterface;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Provides a 'Hello' Block
+ * Provides a 'Hello' Block.
  *
  * @Block (
  *  id = "hello_block",
- *  admin_label = @Translation("Hello block"),   
+ *  admin_label = @Translation("Hello block"),
  * )
  */
 class HelloBlock extends BlockBase implements BlockPluginInterface {
-    /**
-     * {@inheritdoc}
-     */
-    public function build() {
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
 
-        $config = $this->getConfiguration();
+    $config = $this->getConfiguration();
 
-        return array (
-            '#markup' => $this->t('Hello, World and @name!', 
-                array('@name' => $config['name'])),
-            '#attached' => array(
-                'library' => array(
-                    'hello_world/hello'
-                    )
-                )
-        );
-    }
+    return array(
+      '#markup'   => $this->t('Hello, World and @name!',
+        array('@name' => $config['name'])),
+      '#attached' => array(
+        'library' => array(
+          'hello_world/hello',
+        ),
+      ),
+    );
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function blockForm($form, FormStateInterface $form_state) {
-        $form = parent::blockForm($form, $form_state);
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
 
-        $config = $this->getConfiguration();
+    $config = $this->getConfiguration();
 
-        $form['hello_block_name'] = array(
-            '#type' => 'textfield',
-            '#title' => $this->t('Who'),
-            '#description' => $this->t('Who do you want to say hello to?'),
-            '#default_value' => isset($config['name']) ? $config['name'] : '',
-        );
+    $form['hello_block_name'] = array(
+      '#type'          => 'textfield',
+      '#title'         => $this->t('Who'),
+      '#description'   => $this->t('Who do you want to say hello to?'),
+      '#default_value' => isset($config['name']) ? $config['name'] : '',
+    );
 
-        return $form;
-    }
+    return $form;
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function blockSubmit($form, FormStateInterface $form_state) {
-        $this->setConfigurationValue('name', $form_state->getValue('hello_block_name'));
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->setConfigurationValue('name', $form_state->getValue('hello_block_name'));
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function defaultConfiguration() {
-        $default_config = \Drupal::config('hello_world.settings');
-        return array(
-            'name' => $default_config->get('default_name')
-        );
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    $default_config = \Drupal::config('hello_world.settings');
+    return array(
+      'name' => $default_config->get('default_name'),
+    );
+  }
+
 }
